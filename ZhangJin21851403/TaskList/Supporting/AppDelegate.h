4c98d62414d21679ae930d00d7feef0144f288ed
Zhang Jin <zj_960408@163.com>
2018-11-01 09:40:04 +0800

    remove .DS_Store

---
+++ ZhangJin21851403/TaskList/Supporting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaskList
+//
+//  Created by ZJ on 2018/10/28.
+//  Copyright © 2018 JZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
